736faf35299d4352942c713e57e4c95a2e5527ed
mojun <931554665@qq.com>
2022-07-01 21:15:01 +0800

    master

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <el-container style="height: 100%;">
+      <el-aside style="width: auto;height: 100vh;">
+        <NavbarVue />
+      </el-aside>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+    <!-- style="margin-left:64px"  -->
+
+  </div>
+</template>
+<script>
+import NavbarVue from "./components/Sidebar.vue";
+export default {
+  components: {
+    NavbarVue
+  }
+}
+
+</script>
+<style lang="less">
+.el-card__body, .el-main{
+  padding: 0 !important;
+}
+</style>
